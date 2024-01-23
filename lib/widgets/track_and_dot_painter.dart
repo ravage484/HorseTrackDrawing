b0f48143313f04effa168d35888842973835dd5e
ravage484 <rasmorg@gmail.com>
2024-01-23 00:09:52 -0500

    Added functionality for many dots to animate on the line

--- lib/widgets/track_and_dot_painter.dart
+++ lib/widgets/track_and_dot_painter.dart
@@ -3,11 +3,10 @@ import 'package:horse_track_drawing/models/dot.dart';
 import 'package:horse_track_drawing/widgets/custom_painter.dart';
 
 // Custom painter class for drawing the track and animated dot
-class TrackAndDotPainter extends BaseCustomPainter {
-  final double progress; // The progress of the animation
-  final Dot dot; // The dot to be animated
+class TrackAndDotPainter extends CustomPainter {
+  final List<Dot> dots;
 
-  TrackAndDotPainter({required this.progress, required this.dot});
+  TrackAndDotPainter({required this.dots});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -15,7 +14,7 @@ class TrackAndDotPainter extends BaseCustomPainter {
     final trackOutlinePaint = Paint()
       ..color = Colors.brown
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 40.0;
+      ..strokeWidth = 60.0;
 
     // Paint for the track fill
     final trackFillPaint = Paint()
@@ -36,63 +35,36 @@ class TrackAndDotPainter extends BaseCustomPainter {
     
     // Draw the track outline
     canvas.drawPath(trackPath, trackOutlinePaint);
-
-    // // Draw the lines to represent lanes in the track and the finish line
-    // final lanePaint = Paint()
-    //   ..color = Colors.white
-    //   ..style = PaintingStyle.stroke
-    //   ..strokeWidth = 1.0;
-
-    // // Also define the offset for each lane by roughly dividing the width of the track
-    // final laneWidthOffset = size.width * 0.025;
-    // final laneHeightOffset = size.height * 0.07;
-
-    // // Define the track path (several lanes as rounded rectangles)
-    // final outerLanePath = Path()
-    //   ..addRRect(RRect.fromRectAndRadius(
-    //   Rect.fromCenter(center: centerOffset, width: size.width + laneWidthOffset, height: size.height + laneHeightOffset),
-    //     Radius.circular(size.height / 2),
-    //   ));
-    // final lanePath = Path()
-    //   ..addRRect(RRect.fromRectAndRadius(
-    //     Rect.fromLTWH(0, 0, size.width, size.height),
-    //     Radius.circular(size.height / 2),
-    //   ));
-    // final innerLanePath = Path()
-    //   ..addRRect(RRect.fromRectAndRadius(
-    //   Rect.fromCenter(center: centerOffset, width: size.width - laneWidthOffset, height: size.height - laneHeightOffset),
-    //     Radius.circular(size.height / 2),
-    //   ));
-
-    // // Draw the lanes
-    // canvas.drawPath(lanePath, lanePaint);
-    // canvas.drawPath(outerLanePath, lanePaint);
-    // canvas.drawPath(innerLanePath, lanePaint);
     
-    // Draw the animated dot with a white outline
-    final dotPaint = Paint()..color = dot.color;
-    final outlinePaint = Paint()..color = Colors.white;
-    const dotRadius = 10.0;
-    const outlineWidth = 4.0;
-    final pathMetrics = trackPath.computeMetrics();
-    final metric = pathMetrics.first;
+    // Paint each animated dot with white outlines
+    for (var dot in dots) {
+      // Get the current progress of the animation
+      final progress = dot.controller.value;
 
-    // Calculate the position of the dot based on the progress of the animation
-    final dotPath = metric.extractPath(
-      0.0,
-      metric.length * progress,
-    );
-    final dotPosition = dotPath.computeMetrics().first.getTangentForOffset(dotPath.computeMetrics().first.length)?.position ?? Offset.zero;
-    
-    // Draw the outline
-    canvas.drawCircle(dotPosition, dotRadius + outlineWidth, outlinePaint);
+      // Get the dot's properties
+      final dotPaint = Paint()..color = dot.color;
+      final outlinePaint = Paint()..color = Colors.white;
+      const dotRadius = 10.0;
+      const outlineWidth = 4.0;
+      final pathMetrics = trackPath.computeMetrics();
+      final metric = pathMetrics.first;
 
-    // Draw the dot
-    canvas.drawCircle(dotPosition, dotRadius, dotPaint);
+      // Calculate the position of the dot based on the progress of the animation
+      final dotPath = metric.extractPath(
+        0.0,
+        metric.length * progress,
+      );
+      final dotPosition = dotPath.computeMetrics().first.getTangentForOffset(dotPath.computeMetrics().first.length)?.position ?? Offset.zero;
+      
+      // Draw the outline
+      canvas.drawCircle(dotPosition, dotRadius + outlineWidth, outlinePaint);
+
+      // Draw the dot
+      canvas.drawCircle(dotPosition, dotRadius, dotPaint);
+    }
   }
 
   @override
-  bool shouldRepaint(covariant TrackAndDotPainter oldDelegate) {
-    return oldDelegate.progress != progress || oldDelegate.dot != dot;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
 }
